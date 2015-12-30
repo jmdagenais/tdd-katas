da5eae70448d265ead4096702bf30e52f8b3a074
JMD <jmdagenais@hotmail.ca>
2015-12-30 09:30:38 -0500

    ajout de la gestion des valeurs de 10 à 19 et des nombres 7x et 9x

--- nombreEnMots/nombreEnMots.js
+++ nombreEnMots/nombreEnMots.js
@@ -2,25 +2,39 @@ var numberInWords = {
 
   digit_value: ['', 'un', 'deux', 'trois', 'quatre', 'cinq', 'six', 'sept', 'huit', 'neuf', 'dix'], //'one', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
 
-  dizaine: ['', '', 'vingt', 'trente', 'quarante', 'cinquante', 'soixante', 'soixante-dix', 'quatre-vingt', 'quatre-vingt-dix'],
+  dizaine: ['', '', 'vingt', 'trente', 'quarante', 'cinquante', 'soixante', 'soixante', 'quatre-vingt', 'quatre-vingt'],
+
+  de10a19: ['dix', 'onze', 'douze', 'treize', 'quatorze', 'quinze', 'seize', 'dix-sept', 'dix-huit', 'dix-neuf'],
 
   convert: function(n){
       var str = n.toString();
       var digits = str.split('');
       var output = '';
+      var unite;
+
       if(n === 0){
           return 'zero';
       }
       if(str.length >= 1){
-          output = this.digit_value[digits.pop()];
+        output = this.digit_value[digits.pop()];
+        unite = output;
       }
 
-      if(str.length >= 2){
-          var unite = output;
-          var dizaine = this.dizaine[digits.pop()];
+      if(n >= 10 && n <= 19){
+        output = this.de10a19[n%10];
+      }
+
+      if(str.length >= 2 && n >= 20){
+          var current_digit = digits.pop();
+          var dizaine = this.dizaine[current_digit];
+          var lien;
           output = dizaine;
-          if(unite){
-            var lien = unite === 'un' ? ' et ' : '-';
+          if(current_digit == '7' || current_digit == '9'){
+            //pour les nombres de la forme 7x et 9x
+            lien = n%10 === 1 ? 'et' : '-';
+            output += lien + this.de10a19[n%10];
+          } else if(unite){
+            lien = unite === 'un' ? ' et ' : '-';
             if(!dizaine){
               lien = '';
             }
--- nombreEnMots/nombreEnMots.spec.js
+++ nombreEnMots/nombreEnMots.spec.js
@@ -7,9 +7,11 @@ TestCase("Number in word test", {
   },
 
   "test two digits number": function(){
+    assertEquals('dix', numberInWords.convert(10));
     assertEquals('quarante', numberInWords.convert(40));
     assertEquals('trente et un', numberInWords.convert(31));
     assertEquals('vingt-deux', numberInWords.convert(22));
+    assertEquals('quatre-vingt-huit', numberInWords.convert(88));
   },
 
   "test three digits number": function(){
@@ -17,5 +19,22 @@ TestCase("Number in word test", {
     assertEquals('trois cents trente-neuf', numberInWords.convert(339));
     assertEquals('trois cents cinq', numberInWords.convert(305));
     assertEquals('six cents', numberInWords.convert(600));
+    assertEquals('cent', numberInWords.convert(100));
+  },
+
+  "test nombre de 11 à 19": function(){
+    assertEquals('onze', numberInWords.convert(11));
+    assertEquals('treize', numberInWords.convert(13));
+    assertEquals('dix-sept', numberInWords.convert(17));
+    assertEquals('dix-neuf', numberInWords.convert(19));
+  },
+
+  "test nombre 7x et 9x": function(){
+    assertEquals('soixante-dix', numberInWords.convert(70));
+    assertEquals('soixante-douze', numberInWords.convert(72));
+    assertEquals('soixante-dix-huit', numberInWords.convert(78));
+    assertEquals('quatre-vingt-dix', numberInWords.convert(90));
+    assertEquals('quatre-vingt-treize', numberInWords.convert(93));
+    assertEquals('quatre-vingt-dix-neuf', numberInWords.convert(99));
   }
 });
